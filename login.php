ec2e906cacd2c594cc92454485f5b9a203b7ecf0
galiiiiiiiiiiiiiiiiiiiiiii <galih.kusuma.fict@krw.horizon.ac.id>
2026-09-23 13:38:45 +0700

    first

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+
+require "koneksi.php";
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$sql = "SELECT nama, password FROM mahasiswa WHERE username = ?";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->bind_param("s", $username);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if ($result->num_rows === 1) {
+
+    $data = $result->fetch_assoc();
+
+    if (password_verify($password, $data["password"])) {
+
+        echo "Selamat Datang, " . $data["nama"];
+    } else {
+
+        echo "Username atau Password salah!";
+    }
+} else {
+
+    echo "Username atau Password salah!";
+}
+
+$stmt->close();
+$conn->close();
